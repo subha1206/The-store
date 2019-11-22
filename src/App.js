14ed00633c04fe6319e14d5db454d7d38003949f
Subha Sarkar <sarkarsubha1206@gmail.com>
2019-11-22 19:53:17 +0530

    Created google signIn and added to the app using Firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ import Header from './components/header/header.component';
 import SigninSignup from './pages/signin-signup/signin-signup.component';
 import HomePage from './pages/homepage/homepage.comonents';
 import ShopPage from './pages/shop/shop.component';
+import { auth } from './firebase/firebase.utils';
 
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SigninSignup} />
-      </switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })    
+     })
+   }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SigninSignup} />
+        </switch>
+      </div>
+    );
+  }
 }
 
 export default App;
