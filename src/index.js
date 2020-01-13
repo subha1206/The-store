4feaef4c11851be12eda40120ecc6a00870979cc
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-01-13 23:01:35 +0530

    Added redux persists package for local storage and to save the cart-item locally in browser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import {store, persistor} from './redux/store'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,7 +11,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor} >
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
