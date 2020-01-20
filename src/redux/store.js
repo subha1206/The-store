3bb8d5431069737765c3dd122505045d16337483
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-01-20 14:53:39 +0530

    optimize for production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import {persistStore} from 'redux-persist'
-import thunkMiddleware from 'redux-thunk'
+
 
 import rootReducer from './root-reducer'
 
-const middlewares = [logger, thunkMiddleware]
+const middlewares = []
+
+if( process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
